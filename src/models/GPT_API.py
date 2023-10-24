c238f812a4b26a9524e832ab5efb305392776e21
Alex Chandler <alex.chandler@utexas.edu>
2023-10-24 14:39:19 -0500

    improved query model to use future to prevent waiting for long timeout

--- src/models/GPT_API.py
+++ src/models/GPT_API.py
@@ -1,5 +1,6 @@
 import openai
 import time
+from concurrent.futures import ThreadPoolExecutor, TimeoutError
 
 def read_api_token(token_path):
     # Read API token from a dedicated file
@@ -7,36 +8,32 @@ def read_api_token(token_path):
         API_TOKEN = f.read().strip()
     return API_TOKEN
     
-def query_model(model, prompt, model_temperature = .2):
+def query_model(model, prompt, model_temperature=.2, timeout=10):
     message = [{"role": "user", "content": prompt}]
     # Estimate the number of tokens used
     estimated_tokens = len(prompt.split()) * 3
     # Set the rate limits for different models
-    rate_limit = 10_000 if "gpt-4" in model else 90_000
+    rate_limit = 10_000 if "gpt-4" in model else 90_000  
     try_cnt = 0
-    max_try_cnt = 10
+    max_try_cnt = 10  
     while try_cnt < max_try_cnt:
-        try:
-            response = openai.ChatCompletion.create(
-                model=model,
-                messages=message,
-                temperature=model_temperature, # TODO: experiment with temperature
-                # max_tokens=4000,
-                frequency_penalty=0.0
-            )
-            break
-        except Exception as e:
-         # except openai.error.RateLimitError as e:
-            # Calculate the wait time based on the estimated token usage and rate limit
-            wait_time = (estimated_tokens / rate_limit) * 60 * (1+try_cnt/4)
-            print(f"{str(e)} occured. Waiting for {wait_time} seconds...")
-            time.sleep(wait_time)
-            try_cnt +=1
-            continue
-    if try_cnt == max_try_cnt:
-        raise Exception("Errors occurred too many times. Aborting...")
-    text_response = response["choices"][0]["message"]["content"]
-    return text_response
+        with ThreadPoolExecutor() as executor:
+            future = executor.submit(openai.ChatCompletion.create,
+                                     model=model,
+                                     messages=message,
+                                     temperature=model_temperature,
+                                     frequency_penalty=0.0)
+            try:
+                response = future.result(timeout=timeout)
+                text_response = response["choices"][0]["message"]["content"]
+                return text_response
+            except (TimeoutError, Exception) as e:
+                wait_time = (estimated_tokens / rate_limit) * 60 * (1 + try_cnt / 4)
+                print(f"Error {str(e)} occurred. Waiting for {wait_time} seconds...")
+                time.sleep(wait_time)
+                try_cnt += 1
+    
+    raise Exception(f"Errors occurred too many times. Aborting...")
 
 def set_api_key(api_token_path):
     openai.api_key = read_api_token(api_token_path)
