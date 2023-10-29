fd5543b4a69a4588eb585c8dba1d7b8be6417a9d
Rohan Jha <robro612@gmail.com>
2023-10-29 14:18:01 -0500

    Restored Alex's other query_model function in case it's used elsewhere

--- src/models/model_api.py
+++ src/models/model_api.py
@@ -157,6 +157,30 @@ if __name__ == "__main__":
         )
     )
 
+def query_model(model, prompt, model_temperature=.2, timeout=10):
+    message = [{"role": "user", "content": prompt}]
+    # Estimate the number of tokens used
+    estimated_tokens = len(prompt.split()) * 3
+    # Set the rate limits for different models
+    rate_limit = 10_000 if "gpt-4" in model else 90_000  
+    try_cnt = 0
+    max_try_cnt = 10  
+    while try_cnt < max_try_cnt:
+        with ThreadPoolExecutor() as executor:
+            future = executor.submit(openai.ChatCompletion.create,
+                                     model=model,
+                                     messages=message,
+                                     temperature=model_temperature,
+                                     frequency_penalty=0.0)
+            try:
+                response = future.result(timeout=timeout)
+                text_response = response["choices"][0]["message"]["content"]
+                return text_response
+            except (TimeoutError, Exception) as e:
+                wait_time = (estimated_tokens / rate_limit) * 60 * (1 + try_cnt / 4)
+                print(f"Error {str(e)} occurred. Waiting for {wait_time} seconds...")
+                time.sleep(wait_time)
+                try_cnt += 1
 if __name__ == "__main__":
     model = OpenAIModel(
         api_token=read_api_token("data//imported//datasets//api_token.txt"),
